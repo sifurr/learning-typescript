789828e737f1b3c2d0bedceb8502d06e88bd5782
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-05-03 15:46:51 +0600

    Learn Conditional types

---
+++ module-2/conditional_types.ts
@@ -0,0 +1,17 @@
+{
+    type a1 = number;
+    type b1 = undefined;
+
+    type x = a1 extends null ? true : false; // conditional type
+    type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+    type Sheikh = {
+        bike: string;
+        car: string;
+        ship: string;
+    }
+
+    type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+    type HasTractor = CheckVehicle<'tractor'>;
+}
